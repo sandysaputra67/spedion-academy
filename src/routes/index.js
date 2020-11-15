f2f691f6ffca9f309b7beeaa74b8d454f0766d8e
e-compfast <alamhafidz01@gmail.com>
2020-11-15 10:18:31 +0700

    Edit Styles.css dan src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Loader, Header, Footer} from '../components';
 import { Teachers, Students, Login, Main, KursusViews, SeminarOnline, Market, Instruktur } from '../pages';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 const Routes = (props) => {
+
   return (
     <>
 
@@ -10,15 +11,17 @@ const Routes = (props) => {
     	<Router>
         <Header/>
             <Loader/>
-    		<Route exact path='/' component={Main}/>
-            <Route exact path='/masuk' component={Login}/>
-            <Route exact path='/daftar' component={Login}/>
-            <Route exact path='/students' component={Students}/>
-            <Route exact path='/teachers' component={Teachers}/>
-            <Route exact path='/kursus' component={KursusViews}/>
-            <Route exact path='/seminar' component={SeminarOnline}/>
-            <Route exact path='/market' component={Market}/>
-            <Route exact path='/instruktur' component={Instruktur}/>
+            <div style={{ height : '100vh'}}>
+        		<Route exact path='/' component={Main}/>
+                <Route exact path='/masuk' component={Login}/>
+                <Route exact path='/daftar' component={Login}/>
+                <Route exact path='/students' component={Students}/>
+                <Route exact path='/teachers' component={Teachers}/>
+                <Route exact path='/kursus' component={KursusViews}/>
+                <Route exact path='/seminar' component={SeminarOnline}/>
+                <Route exact path='/market' component={Market}/>
+                <Route exact path='/instruktur' component={Instruktur}/>
+            </div>
         <Footer/>
     	</Router>
     </StrictMode>
